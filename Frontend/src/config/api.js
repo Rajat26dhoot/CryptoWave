41245cf75e9ea855aac1c2cf1c96c5e2e5770282
Rajatdhoot26 <2rajatd6@gmail.com>
2025-06-05 10:24:35 +0530

    .

--- Frontend/src/config/api.js
+++ Frontend/src/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = 'https://spring-boot-docker-vn4g.onrender.com';
+export const BASE_URL = 'https://cryptowaveupdate.onrender.com';
 
 
 const api=axios.create({
